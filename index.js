9e9f0237525a261f8cd0bca75ecfda39df48fb42
Mohamed Sharuq <sharuqqqqq97@gmail.com>
2024-10-23 13:04:30 +0000

    update5

--- index.js
+++ index.js
@@ -16,21 +16,37 @@ let connection;
 
 async function main() {
     connection = await createConnection({
-        'host': process.env.DB_HOST, //server
-        'user': process.env.DB_USER, 
+        'host': process.env.DB_HOST,
+        'user': process.env.DB_USER,
         'database': process.env.DB_NAME,
         'password': process.env.DB_PASSWORD
     })
 
     app.get('/customers', async (req, res) => {
-        let [customers] = await connection.execute('SELECT * FROM Customers INNER JOIN Companies ON Customers.company_id = Companies.company_id');
+        let [customers] = await connection.execute({
+            'sql':`
+            SELECT * from Customers
+                JOIN Companies ON Customers.company_id = Companies.company_id;
+            `,
+            nestTables: true
+
+        });
         res.render('customers/index', {
             'customers': customers
         })
+        console.log(customers);
     })
 
+    app.get('/customers/create', async(req,res)=>{
+        let [companies] = await connection.execute('SELECT * from Companies');
+        res.render('customers/create', {
+            'companies': companies
+        })
+    })
+    
     app.get('/', (req,res) => {
-        res.send('Hello, World!');
+        res.redirect("customers");
+        //res.send('Hello, World!');
     });
 
     app.listen(3000, ()=>{
