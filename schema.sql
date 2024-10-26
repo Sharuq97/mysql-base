ffcccfe8ecec174d38a0f632786a031c48c21770
Mohamed Sharuq <sharuqqqqq97@gmail.com>
2024-10-26 08:57:28 +0000

    update13

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    rating INT,
+    rating FLOAT,
     company_id INT,
     FOREIGN KEY (company_id) REFERENCES Companies(company_id)
 );
